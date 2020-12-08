9e37a9b1b943eaef86613c1c1d5b011b68b305db
Ivan Vladimirovich Konyukhov <konyukhovivan@yandex.ru>
2020-12-08 09:51:53 +0300

    Initial commit

---
+++ src/iu/konyukhov/Main.java
@@ -0,0 +1,13 @@
+package iu.konyukhov;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    hello();
+    }
+
+    static void hello()
+    {
+        System.out.println("Hello!");
+    }
+}
